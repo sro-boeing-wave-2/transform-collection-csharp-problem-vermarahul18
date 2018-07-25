8eedb5b1d0cb8be3a08eaed64f6fdb5cff22c646
Rahul <vermarahulkps@gmail.com>
2018-07-25 11:02:37 +0530

    changes after review

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,17 +6,9 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static List<int> Map(this int[] array,Func<int,int> Operation)
+        public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> element, Func<TSource, TResult> operation)
         {
-            List<int> li = new List<int>(); 
-            foreach (int x in array) {
-                li.Add(Operation(x));
-            }
-            return li;
- // public static TEnumerable<TResult> Map<TSource,TResult>(this IEnumerable<TSource> x,Func<TSource,TResult> operation)
- //return x.Select(transformer);
-
-            throw new NotImplementedException();
+           return element.Select(operation);
         }
     }
 }
